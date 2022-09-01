260df6ecebba93ab3cdff6466338813ce5e462f1
Alex <levchenko.a.v@mail.ru>
2022-09-01 16:51:06 +0300

    add module Natasha

--- parse_csv.py
+++ parse_csv.py
@@ -2,7 +2,10 @@
 
 import csv
 import io
+from operator import index
 import shutil
+from natasha import NamesExtractor, MorphVocab
+
 
 path_csv = 'D:\\python\\test1\\test_data.csv'
 path_test_data = shutil.copy(path_csv, 'D:\\python\\test1\\data.csv')
@@ -12,16 +15,29 @@ greeting = ['здравствуйте', 'добрый день']
 bye = ['до свидания', 'всего доброго']
 my_name = ['меня зовут']
 
+morph_vocab = MorphVocab()
+extractor = NamesExtractor(morph_vocab)
+# matches = extractor(text)
+# for match in matches:
+#     start, stop = match.span
+#     print(start, stop, text[start:stop])
+#     print(match.fact)
+#     print()
 
 with io.open(path_test_data, newline='', encoding='utf-8') as f:
     reader = csv.reader(f)
     for rows in reader:
+        matches = extractor(rows)
+
+        print(str(matches)) # нужно откорректировать данный вывод. Не работает.
         list_lower = [row.lower() for row in rows]
-        if list_lower[2] == 'manager':
-            for greet in greeting:
-                if greet in list_lower[3]:
-                    print(list_lower)
-            for b in bye:
-                if b in list_lower[3]:
-                    print(list_lower)
+        if my_name[0] in list_lower[3]:
+            print(list_lower)
+        # if list_lower[2] == 'manager':
+        #     for greet in greeting:
+        #         if greet in list_lower[3]:
+        #             print(list_lower)
+        #     for b in bye:
+        #         if b in list_lower[3]:
+        #             print(list_lower)
 
